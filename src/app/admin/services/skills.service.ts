e07c264522fea63eed01beffeb901def6eaac0c0
atlom <jaaf27@hotmail.com>
2021-06-18 22:41:41 -0400

    Skill Service agregar y mostar

--- src/app/admin/services/skills.service.ts
+++ src/app/admin/services/skills.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Skills } from 'src/app/core/shared/models/portafolio.models';
+import { Skill, Skills } from 'src/app/core/shared/models/portafolio.models';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +8,18 @@ import { Skills } from 'src/app/core/shared/models/portafolio.models';
 export class SkillsService {
 
   protected api:string = "http://18.117.197.8:3000/api/"
-
   constructor(private http: HttpClient) { }
 
   obtenerSkills(){
     return this.http.get<Skills>(`${this.api}skill`)    
   }
 
+  agregarSkill(skill: Skill){
+
+    let header = new HttpHeaders()
+    header.append('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjp7Il9pZCI6IjYwYmQzODMwNzE5YTk5MTVjYzcwODEzZCIsIm5vbWJyZSI6ImVsZWN0aXZhMyIsImVtYWlsIjoiZWxlY3RpdmEzQGVsZWN0aXZhMy5jb20iLCJwYXNzd29yZCI6IiQyYSQxMCRQbGNlRW9pMEQxYXY1QWhsZ2x0NVNPSjdReTJvSWoweUthc0YwV2UuWEJxZEZFbmR5VlhlMiIsIl9fdiI6MH0sImlhdCI6MTYyNDA2ODU0NiwiZXhwIjoxNjI0MDcyMTQ2fQ.zUCyrT0iBad-TPHqW70gyzSC4SqGZkQyqs5BiVnSTj0');
+    return this.http.post(`${this.api}skill`,skill,{
+      headers: header
+    })
+  }
 }
--- src/app/admin/skill/skill.component.ts
+++ src/app/admin/skill/skill.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Skill, Skills } from 'src/app/core/shared/models/portafolio.models';
 import { SkillsService } from '../services/skills.service';
 
@@ -10,18 +11,29 @@ import { SkillsService } from '../services/skills.service';
 })
 export class SkillComponent implements OnInit {
 
-  public skill: Skill | undefined;
-
-  public skills: any = [];
-
+  public skills: Array<Skill> = [];
 
   verForm: boolean = false;
+  imgURL: string = '';
 
-  constructor(private skillDao: SkillsService) { }
+  skillForm: FormGroup;
+
+  constructor(private skillDao: SkillsService,
+    private fb: FormBuilder) {
+      this.skillForm = this.fb.group({
+        img: [null],
+        titulo: ['',[Validators.required, Validators.minLength(2)]]        
+      })
+     }
 
   ngOnInit(): void {
     this.skillDao.obtenerSkills()
-      .subscribe(skills => this.skills = skills)
+      .subscribe(skills => {
+        this.skills = skills.skills
+      },
+      error => {
+        console.log(error)        
+      });
   }
 
   mostrarForm(){
@@ -31,5 +43,42 @@ export class SkillComponent implements OnInit {
   cancelar(){
     this.verForm = !this.verForm
   }
+
+  nuevaSkill(){
+    if (this.skillForm.get('titulo')?.invalid && (this.skillForm.get('titulo')?.dirty || this.skillForm.get('titulo')?.touched )) return
+
+    let model: Skill = {
+      titulo: this.skillForm.get('titulo')?.value,
+      img: this.skillForm.get('img')?.value
+    }
+
+    this.skillDao.agregarSkill(model)
+      .subscribe(res => console.log(res),
+        error => console.log(error))
+  }
+
+  verSkill(skill: Skill){
+    console.log(skill)
+  }
+
+  mostraImagen(event:any){
+    const file = (event.target as HTMLInputElement).files![0]; 
+    
+    if(file == null) return
+
+    this.skillForm.patchValue({
+      img: file
+    })
+
+    this.skillForm.get('img')?.updateValueAndValidity();
+    
+    const reader = new FileReader();
+    reader.onload = () =>{
+      this.imgURL = reader.result as string;
+    }
+    reader.readAsDataURL(file);
+  }
+
+
 
 }
