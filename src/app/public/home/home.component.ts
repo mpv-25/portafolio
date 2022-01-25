5f74a53c44e16c56557f070db89ad2e5a5e606a6
Matias Pinto <matias25pinto@gmail.com>
2022-01-25 16:45:03 -0300

    ADD(environment):URL de producción

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { PortafolioService } from '../../core/shared/services/portafolio.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  constructor() {
+  constructor(private portafolioService: PortafolioService) {
+    this.portafolioService.getSkills().subscribe((resp) => {});
   }
 
   ngOnInit(): void {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiPortafolio:
-    'http://ec2-18-117-197-8.us-east-2.compute.amazonaws.com:3000',
+  apiPortafolio: 'https://api-matias-pinto.herokuapp.com',
 };
